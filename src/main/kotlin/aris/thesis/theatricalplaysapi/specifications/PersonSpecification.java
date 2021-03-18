a1b63774974dadae9d88416f211edfb43dba2f68
ar1st <aristnm@hotmail.com>
2021-03-19 00:15:16 +0200

    cleaned code

--- src/main/kotlin/aris/thesis/theatricalplaysapi/specifications/PersonSpecification.java
+++ src/main/kotlin/aris/thesis/theatricalplaysapi/specifications/PersonSpecification.java
@@ -2,16 +2,18 @@ package aris.thesis.theatricalplaysapi.specifications;
 
 import aris.thesis.theatricalplaysapi.entities.Person;
 import aris.thesis.theatricalplaysapi.specifications.base.SearchCriteria;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.domain.Specification;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import static aris.thesis.theatricalplaysapi.exceptions.error.ErrorsKt.wrongQuery;
+
 
 public class PersonSpecification implements Specification<Person> {
-    private SearchCriteria criteria;
+    private final SearchCriteria criteria;
 
     public PersonSpecification(SearchCriteria searchCriteria) {
         this.criteria = searchCriteria;
@@ -19,23 +21,24 @@ public class PersonSpecification implements Specification<Person> {
 
     @Override
     public Predicate toPredicate(
-            Root<Person> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-
+            @NotNull Root<Person> root, @NotNull CriteriaQuery<?> query, @NotNull CriteriaBuilder builder) {
+        //todo remove unused operations
         switch (criteria.getOperation()) {
             case EQUALITY:
                 return builder.equal(root.get(criteria.getKey()), criteria.getValue());
             case NEGATION:
                 return builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
             case GREATER_THAN:
-                return builder.greaterThan(root.<String> get(
+                return builder.greaterThan(root.get(
                         criteria.getKey()), criteria.getValue().toString());
             case LESS_THAN:
-                return builder.lessThan(root.<String> get(
+                return builder.lessThan(root.get(
                         criteria.getKey()), criteria.getValue().toString());
             case LIKE:
-                return builder.like(root.<String> get(
+                return builder.like(root.get(
                         criteria.getKey()),"%" +  criteria.getValue().toString()  + "%");
             default:
+                wrongQuery();
                 return null;
         }
     }
