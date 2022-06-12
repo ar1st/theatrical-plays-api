bdbb74c3d67cab1801ad9490564bbb42cd179c68
Aristeidis Tsachlaris <aristnm@hotmail.com>
2022-06-12 11:30:11 +0300

    Add autowired annotation

--- src/main/kotlin/aris/thesis/theatricalplaysapi/metrics/MetricFilter.java
+++ src/main/kotlin/aris/thesis/theatricalplaysapi/metrics/MetricFilter.java
@@ -1,5 +1,6 @@
 package aris.thesis.theatricalplaysapi.metrics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -13,6 +14,7 @@ public class MetricFilter implements Filter {
 
     private MetricsService metricService;
 
+    @Autowired
     public MetricFilter(MetricsService metricService) {
         this.metricService = metricService;
     }
