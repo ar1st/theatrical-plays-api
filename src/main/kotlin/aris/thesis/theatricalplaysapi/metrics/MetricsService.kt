eb083a21251224d9c86d69d6245d7535a724cccb
Aristeidis Tsachlaris <aristnm@hotmail.com>
2022-06-13 11:17:02 +0300

    Remove metrics endpoint

--- src/main/kotlin/aris/thesis/theatricalplaysapi/metrics/MetricsService.kt
+++
@@ -1,9 +0,0 @@
-package aris.thesis.theatricalplaysapi.metrics
-
-import aris.thesis.theatricalplaysapi.services.proto.ModelService
-
-interface MetricsService: ModelService {
-    fun increaseCount(request: String?, status: Int)
-
-    fun getGraphData(): Array<out Array<Any>>?
-}
